7560d4a47ae342090d8925029a146ca7946fc6b9
LUKAS BELAJ <lukasbelaj@gmail.com>
2016-10-14 19:25:56 +0200

    commit try no2

--- src/main/FirstTry.java
+++ src/main/FirstTry.java
@@ -6,9 +6,10 @@ package main;
 public class FirstTry {
     public static void main(String[] args) {
         System.out.println("Ahoj");
-        ThreadTest tt1 = new ThreadTest("SuperVlakno");
+        ThreadTest tt1 = new ThreadTest("Vlakno");
         tt1.start();
         ThreadTest tt2 = new ThreadTest("SuperVlakno");
         tt2.start();
+        System.out.println("The End");
     }
 }
