d9440aef0a977746c19b5d7c12d7b9595aa36b71
LUKAS BELAJ <lukasbelaj@gmail.com>
2016-10-20 17:22:48 +0200

    Rozchodene pomocou locku

--- src/main/FirstTry.java
+++ src/main/FirstTry.java
@@ -1,23 +1,46 @@
 package main;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by lukas on 22.09.2016.
  */
 public class FirstTry {
 
     public static int actualCol;
+    public static ReentrantLock lock = new ReentrantLock();
     public static void main(String[] args) {
 
         actualCol=1;
-        System.out.println("Ahoj");
-        Thread1 tt1 = new Thread1("Vlakno");
-        tt1.start();
-        Thread1 tt2 = new Thread1("SuperVlakno0");
-        tt2.start();
-        Thread1 tt3 = new Thread1("SuperVlakno1");
-        tt3.start();
-        Thread1 tt4 = new Thread1("SuperVlakno2");
-        tt4.start();
-        System.out.println("The End");
+//        System.out.println("Ahoj");
+//        Thread1 tt1 = new Thread1("Vlakno");
+//        tt1.start();
+//        Thread1 tt2 = new Thread1("SuperVlakno0");
+//        tt2.start();
+//        Thread1 tt3 = new Thread1("SuperVlakno1");
+//        tt3.start();
+//        Thread1 tt4 = new Thread1("SuperVlakno2");
+//        tt4.start();
+        for(int i=0;i<10;i++){
+            new Thread1("SuperVlakno"+i).start();
+        }
+        //System.out.println("The End");
+    }
+
+    public static int semafor(String threadName){
+        //System.out.println(FirstTry.actualCol);
+        FirstTry.lock.lock();
+        if (FirstTry.actualCol == 1){
+            System.out.println("zelena");
+            FirstTry.actualCol=0;
+        }
+        else{
+            System.out.println("cervena");
+            FirstTry.actualCol=1;
+        }
+        //System.out.println("Vlakno pise: " + threadName);
+        FirstTry.lock.unlock();
+        return FirstTry.actualCol;
+
     }
 }
--- src/main/Thread1.java
+++ src/main/Thread1.java
@@ -7,46 +7,34 @@ public class Thread1 implements Runnable {
 
     protected Thread t;
     protected String threadName;
-    protected int actualCol;
+    //protected int actualCol;
 
     Thread1(String name){
         threadName=name;
-        System.out.println("Vytvara sa: "+threadName);
+        //System.out.println("Vytvara sa: "+threadName);
     }
 
     public void run() {
 
         try {
-                semafor();
+            FirstTry.semafor(threadName);
                 Thread.sleep(50);
 
         } catch (InterruptedException e) {
             System.out.println("Vlakno sa pokazilo: " + threadName);
         }
-        System.out.println("Vlakno konci: " + threadName);
+        //System.out.println("Vlakno konci: " + threadName);
     }
 
 
     public void start(){
-        System.out.println("Spusta sa: "+threadName);
+        //System.out.println("Spusta sa: "+threadName);
         if(t==null){
             t=new Thread(this,threadName);
             t.start();
         }
     }
 
-    public synchronized int semafor(){
-        System.out.println(actualCol);
-        if (actualCol == 1){
-            System.out.println("zelena");
-            actualCol=0;
-        }
-        else{
-            System.out.println("cervena");
-            actualCol=1;
-        }
-        System.out.println("Vlakno pise: " + threadName);
-        return actualCol;
-    }
+
 }
 
